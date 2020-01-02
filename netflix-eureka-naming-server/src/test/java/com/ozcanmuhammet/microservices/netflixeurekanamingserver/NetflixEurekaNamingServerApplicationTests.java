d018209516f19a4c8e8cbaeb26d93135d33e1341
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2020-01-02 22:51:35 +0300

    spring microservices

---
+++ netflix-eureka-naming-server/src/test/java/com/ozcanmuhammet/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozcanmuhammet.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
